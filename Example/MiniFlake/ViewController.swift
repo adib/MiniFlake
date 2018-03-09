dcabc4c514e74c8118d6294f0d622bf9dd335d42
Sasmito Adibowo <adib@basil-salad.com>
2018-03-09 10:30:19 +0800

    Adding example ID generator app

--- Example/MiniFlake/ViewController.swift
+++ Example/MiniFlake/ViewController.swift
@@ -2,26 +2,54 @@
 //  ViewController.swift
 //  MiniFlake
 //
-//  Created by adib@clovermail.net on 03/07/2018.
-//  Copyright (c) 2018 adib@clovermail.net. All rights reserved.
+//  Copyright © Sasmito Adibowo
+//  http://cutecoder.org
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import Cocoa
+import MiniFlake
 
 class ViewController: NSViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    // Do any additional setup after loading the view.
-  }
-
-  override var representedObject: Any? {
-    didSet {
-    // Update the view, if already loaded.
+    @IBOutlet weak var generateCountTextField: NSTextField!
+    
+    @IBOutlet var outputTextView: NSTextView!
+    
+    @IBAction func startGenerate(_ sender: Any) {
+        let generateCountStr = generateCountTextField.stringValue
+        guard let generateCount = Int(generateCountStr), generateCount > 0 else {
+            if let window = view.window {
+                let alert = NSAlert()
+                alert.messageText = NSLocalizedString("Please enter a positive number of identifiers to generate", comment: "Error message")
+                alert.addButton(withTitle: NSLocalizedString("Okay", comment: "Error message"))
+                alert.beginSheetModal(for: window, completionHandler: nil)
+            }
+            return
+        }
+        var outputStr = String()
+        outputStr.reserveCapacity(17 *  generateCount)
+        for _ in 0..<generateCount {
+            let value = Thread.current.nextFlakeID()
+            outputStr.append("\(value)\n")
+        }
+        outputTextView.string = outputStr
     }
-  }
-
-
 }
 
